e5e1d528d8592eb359f9d6a2dc9a376c995718f6
Rodrigo Alba <rodrigoqalba@gmail.com>
2021-09-10 15:41:03 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+let Josh
+
+function introduction(name){
+    return(`Hi, my name is ${name}.`)
+}
+introduction('Josh');
+
+function
+introductionWithLanguage(name= "Josh", language= "Ember.js") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+const name = Gracie
+function introductionWithLanguageOptional (name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
